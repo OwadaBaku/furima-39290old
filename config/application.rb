7ad8da1a79517d754af346c872d28fb1de1f1b71
OwadaBaku <82127461+OwadaBaku@users.noreply.github.com>
2023-05-31 15:49:46 +0900

    Update application.rb

--- config/application.rb
+++ config/application.rb
@@ -17,6 +17,3 @@ module Furima39290
     # the framework and any gems in your application.
   end
 end
-config.middleware.use Rack::Auth::Basic do |username, password|
-  username == ENV['BASIC_AUTH_USERNAME'] && password == ENV['BASIC_AUTH_PASSWORD']
-end
